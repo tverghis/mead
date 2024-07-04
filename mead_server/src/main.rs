577d6df2512a3202b4b6209510fa77f93df999f2
Tarun Verghis <tverghis@greyhound.tverghis.space>
2024-07-04 12:43:50 -0700

    Remove code for non-x86_64-linux targets

--- mead_server/src/main.rs
+++ mead_server/src/main.rs
@@ -1,19 +1,12 @@
-#[cfg(all(target_os = "linux", target_arch = "x86_64"))]
 use libbpf_rs::query::{ProgInfoIter, ProgInfoQueryOptions};
 
 fn main() {
     do_main();
 }
 
-#[cfg(all(target_os = "linux", target_arch = "x86_64"))]
 fn do_main() {
     let iter = ProgInfoIter::with_query_opts(ProgInfoQueryOptions::default().include_all());
     for prog in iter {
         println!("{} (type: {})", prog.name.to_string_lossy(), prog.ty);
     }
-}
-
-#[cfg(any(not(target_os = "linux"), not(target_arch = "x86_64")))]
-fn do_main() {
-    eprintln!("This operating system and/or architecture is not supported.");
 }
