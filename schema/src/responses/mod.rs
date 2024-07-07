f54a4458130587c5838900d56bd8b657d9c142fd
Tarun Verghis <tarun.verghis@gmail.com>
2024-07-07 14:27:17 -0700

    Return tag data from the API

--- schema/src/responses/mod.rs
+++ schema/src/responses/mod.rs
@@ -7,14 +7,20 @@ pub struct ProgInfoResponse {
     #[serde(rename = "type")]
     pub ty: String,
     pub id: u32,
+    pub tag: String,
 }
 
 impl From<ProgramInfo> for ProgInfoResponse {
     fn from(info: ProgramInfo) -> Self {
+        // Convert the bytes in .tag to a hex string
+        let tag = info.tag.0.map(|byte| format!("{:02x?}", byte)).join("");
+
         Self {
             name: info.name.to_string_lossy().to_string(),
-            ty: info.ty.to_string(),
+            // TODO: This should probably be something other than the `Debug` impl, but `Dispay` is no longer implemented for `ProgramType`.
+            ty: format!("{:?}", info.ty),
             id: info.id,
+            tag,
         }
     }
 }
